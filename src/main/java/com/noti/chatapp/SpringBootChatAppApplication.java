630c0fa8dce3800c16fd99fb5df210cc82741a7c
yeriAhn <yeri01080108@gmail.com>
2021-03-18 01:24:47 +0900

    Initial commit

---
+++ src/main/java/com/noti/chatapp/SpringBootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.noti.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noti/chatapp/SpringBootChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noti.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
